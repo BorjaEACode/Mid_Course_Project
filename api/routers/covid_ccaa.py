48c1286a4b9adc56c2ed29ac184dea09647178d7
Borja <espesborja@gmail.com>
2021-12-30 21:13:43 +0100

    1.2.3 Arreglos en router ccaa

--- api/routers/covid_ccaa.py
+++ api/routers/covid_ccaa.py
@@ -16,14 +16,14 @@ def get_ccaa_list():
     results = list(db["Covid_Comunidades"].find({},{"Comunidad autónoma":1,"_id":0}))
     return loads(json_util.dumps(results))
 
-@router.get("/covid_ccaa/{ccaa}")
-def get_ccaa_data(ccaa):
-    results = list(db["Covid_Comunidades"].find({"Comunidad autónoma": ccaa},{"_id":0}))
-    return loads(json_util.dumps(results))
-
 @router.get("/covid_ccaa/most_complete_vaccinated")
 def get_ccaas_most_compl_vac():
     results = list(db["Covid_Comunidades"].find({},{"Comunidad autónoma": 1, "Porcentaje Pauta Completa": 1,"_id": 0}).sort("Porcentaje Pauta Completa", 1))
+    return loads(json_util.dumps(results))
+
+@router.get("/covid_ccaa/{ccaa}")
+def get_ccaa_data(ccaa):
+    results = list(db["Covid_Comunidades"].find({"Comunidad autónoma": ccaa},{"_id":0}))
     return loads(json_util.dumps(results))
 
 @router.get("/covid_ccaa/{vaccine}/{positions}")
