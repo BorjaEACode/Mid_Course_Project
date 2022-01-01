2b0a5ee814f187bbf132561315d916a5bbfcd0b7
Borja <espesborja@gmail.com>
2022-01-01 19:03:18 +0100

    1.3 Alguna visualizacion mas en streamtlit

--- dashboard/main.py
+++ dashboard/main.py
@@ -8,7 +8,7 @@ import folium
 st.title("Covid-19 Dashboard")
 st.text("This is a streamlit display for visualizing data about Covid-19")
 
-chosen_country = st.selectbox("Select one country", [country["Country/Region"] for country in get_country_list()])
+chosen_country = st.selectbox("Select country", [country["Country/Region"] for country in get_country_list()])
 chosen_data = st.selectbox("Select data type", ["Cases","Deaths","Recovered"])
 
 data_country = get_country_data(chosen_country, chosen_data)
@@ -16,15 +16,20 @@ coord_country = get_country_coord(chosen_country)
 
 st.text("")
 st.text("Its coordinates are: ")
-st.text(data_country)
-
-st.text("")
-st.text("Its data is: ")
 st.text(coord_country)
 
 st.text("")
-st.line_chart(data=data_country)
+st.text("Its data is: ")
+st.text(data_country)
 
-m = folium.Map(location=[0,0], zoom_start=16)
-folium.Marker([0,0], popup=chosen_country, tooltip=chosen_country).add_to(m)
+st.header("Data Graph")
+st.text("")
+
+st.line_chart(data=data_country[0].values())
+
+st.header("Data Map")
+st.text("")
+
+m = folium.Map(location=(coord_country[0]["Lat"],coord_country[0]["Long"]), zoom_start=4)
+folium.Marker((coord_country[0]["Lat"],coord_country[0]["Long"]), popup=chosen_country, tooltip=(chosen_country,data_country[0]["4/10/21"],chosen_data)).add_to(m)
 folium_static(m)
