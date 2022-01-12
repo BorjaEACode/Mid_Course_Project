bbdcddec6bde98b8edba31758fbe08ac09ded630
BorjaEACode <92452326+BorjaEACode@users.noreply.github.com>
2022-01-13 00:12:20 +0100

    Update get_communities.py

--- dashboard/data/get_communities.py
+++ dashboard/data/get_communities.py
@@ -1,11 +1,11 @@
-from config.api import url
+from config.api import URL
 import requests
 
 def get_ccaa_list():
-    return requests.get(url+"/covid_ccaa/list").json()
+    return requests.get(URL+"/covid_ccaa/list").json()
 
 def get_ccaa_full_data(ccaa):
-    return requests.get(url+f"/covid_ccaa/full_data/{ccaa}").json()
+    return requests.get(URL+f"/covid_ccaa/full_data/{ccaa}").json()
 
 def get_ccaa_basic_data(ccaa):
-    return requests.get(url+f"/covid_ccaa/basic_data/{ccaa}").json()
+    return requests.get(URL+f"/covid_ccaa/basic_data/{ccaa}").json()
